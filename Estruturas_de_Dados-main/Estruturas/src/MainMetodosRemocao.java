b6d7a1b85b3a1544ac310277edbb84415a102729
JoaoGFB <156378239+JoaoGFB@users.noreply.github.com>
2025-04-10 23:25:58 -0300

    Classe para teste dos métodos de remoção

---
+++ Estruturas_de_Dados-main/Estruturas/src/MainMetodosRemocao.java
@@ -0,0 +1,28 @@
+import Dinamicas.PilhaDinamicaInteger;
+
+public class MainMetodosRemocao {
+    public static void main(String[] args) {
+        PilhaDinamicaInteger pilha = new PilhaDinamicaInteger();
+        pilha.inserirElemento(4, 0);
+        pilha.inserirElemento(5, 0);
+        pilha.inserirElemento(8, 0);
+        pilha.inserirElemento(9, 0);
+        pilha.inserirElemento(5, 0);
+        pilha.inserirElemento(11, 0);
+        pilha.inserirElemento(6, 0);
+        pilha.inserirElemento(5, 0);
+        pilha.inserirElemento(19, 0);
+        pilha.inserirElemento(56, 0);
+        pilha.inserirElemento(5, 0);
+        pilha.exibir();
+
+        //pilha.removerElemento(5);
+
+        //pilha.removerTodasOcorrencias(5);
+
+        //List<Integer> elementosLista = Arrays.asList(4, 5, 9);
+        //pilha.removerSequencia(elementosLista);
+
+        pilha.exibir();
+    }
+}
